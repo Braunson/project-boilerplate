ed95de99471adefc09909b22534802f0b4080cb3
Braunson Yager <Braunson@users.noreply.github.com>
2017-02-13 20:05:55 -0500

    Added lavalog (colorized log)
    Thanks to @johnf on the laracasts #tools Slack channel for the code!

--- .homestead/after.sh
+++ .homestead/after.sh
@@ -12,3 +12,9 @@ echo "\n--- Downloading and installing phpMyAdmin 4.6.6 ---\n"
 curl -sS https://files.phpmyadmin.net/phpMyAdmin/4.6.6/phpMyAdmin-4.6.6-english.tar.gz -o phpmyadmin.tar.gz
 tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 rm phpmyadmin.tar.gz
+
+# Install Lavalog
+# By default convert the line endings just in case
+sed -i -e 's/\r$//' /home/vagrant/project/.homestead/lavalog
+cp /home/vagrant/project/.homestead/lavalog /usr/local/bin/lavalog
+chmod 755 /usr/local/bin/lavalog
