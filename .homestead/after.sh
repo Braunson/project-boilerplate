c68b35894a4f5a116ee497d2feb0cac3af52cce6
Braunson Yager <braunson@gmail.com>
2020-06-23 20:52:42 -0400

    Removed local PMA, updated aliases to be localized
    - Updated aliases to localized versions
    - Updated after.sh with some more commands
    - Removed old PMA method and added new one-liner script
    - Updated Homestead dist file to be cleaner
    - Removed any old PMA stubs or placeholders

--- .homestead/after.sh
+++ .homestead/after.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 
 # Install PHPMyAdmin
 echo 'installing PHPMyAdmin'
-sh /home/vagrant/code/.homestead/pma-setup.sh
+curl -s -S https://gist.githubusercontent.com/Braunson/f7205b83bef1acfb2fe833fdefd30271/raw/phpmyadmin.sh | bash -s phpmyadmin.local
 
 # Install Lavalog
 # By default convert the line endings just in case
@@ -19,11 +19,27 @@ chmod 755 /usr/local/bin/lavalog
 
 # Install some global tools we need
 # All tools from http://jenkins-php.org/installation.html
-composer global require friendsofphp/php-cs-fixer "squizlabs/php_codesniffer=*" phpunit/phpunit phploc/phploc pdepend/pdepend phpmd/phpmd sebastian/phpcpd theseer/phpdox hirak/prestissimo infection/infection sarfraznawaz2005/composer-cost --quiet --no-interaction
+# Removed theseer/phpdox due to deprecated requirements
+# Removed sebastian/phpcpd due to deprecated requirements
+composer global require friendsofphp/php-cs-fixer "squizlabs/php_codesniffer=*" phpunit/phpunit phploc/phploc pdepend/pdepend phpmd/phpmd hirak/prestissimo infection/infection sarfraznawaz2005/composer-cost --quiet --no-interaction
+
+# Adjust some PHP configuration
+# for i in /etc/php/7.4/fpm/php.ini;
+# do
+# 	sudo sed -i 's|memory_limit = 512M|memory_limit  = -1|' $i
+# done
+
+# Adjust CLI PHP Configuration
+# for i in /etc/php/7.4/cli/php.ini;
+# do
+#     sudo sed -i 's|memory_limit = 512M|memory_limit  = -1|' $i
+# done
 
 # Install php-gmp extention/module that we need for WebPush notifications!
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get -y update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install php7.4-gmp
-sudo service php7.4-fpm restart
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install software-properties-common
+# sudo add-apt-repository ppa:ondrej/php
+# sudo apt-get -y update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install php7.4-gmp
+
+# Restart PHP
+# sudo service php7.4-fpm restart
